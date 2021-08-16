8b99c0360c566ce7c40164e172c6fcd0d57b358a
caloja1014 <caloja@espol.edu.ec>
2021-08-16 11:03:23 -0500

    Add field to know if the driver is free

--- include/taxista.h
+++ include/taxista.h
@@ -1,12 +1,14 @@
 #ifndef TAXISTA
 #define TAXISTA
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct taxista_struct
 {
     int id;
     int pos_x;
     int pos_y;
+    bool is_free;
 
 }  Taxista;
 
